4b23554fe500ee15d5c5bd9698b9df3068c92b3f
admin <admin@DESKTOP-RTAJF81.mshome.net>
2024-03-18 08:57:39 +0900

    update

--- boardWeb/src/main/java/co/yedam/common/Acontrol.java
+++ boardWeb/src/main/java/co/yedam/common/Acontrol.java
@@ -6,13 +6,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 public class Acontrol implements Control {
 
 	@Override
 	public void exec(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("A컨트롤이 실행되었습니다.");
-
 	}
 
 }
